71a3c34273dbee718b67195f8fafcacf918820df
Mphoboy <130040248+Mphoboy@users.noreply.github.com>
2024-02-13 16:09:43 +0200

    toolbar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { PrivateComponent } from './layout/private/private.component';
-import { LoginPageComponent } from './pages/signin-page/login-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LandingPageComponent } from './pages/customer/landing-page/landing-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
     children: [
       { path: 'signin', component: LoginPageComponent },
       { path: 'signup', component: SignupComponent },
+      { path: 'home', component: LandingPageComponent },
     ],
   },
 ];
