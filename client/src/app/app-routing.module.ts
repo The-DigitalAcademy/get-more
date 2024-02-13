5b52bf754c99254944ff7c4550a0f3fdc6e43b56
Mphoboy <130040248+Mphoboy@users.noreply.github.com>
2024-02-13 12:01:32 +0200

    sign in fornt end

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { PrivateComponent } from './layout/private/private.component';
 
 const routes: Routes = [
-  { path:'login',component:LoginPageComponent}, 
+  {
+    path: 'private',
+    component: PrivateComponent,
+    children: [{ path: 'login', component: LoginPageComponent }],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
