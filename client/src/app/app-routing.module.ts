76c27f915f00ba02ea278cae3bd8450847d35ba1
Mphoboy <130040248+Mphoboy@users.noreply.github.com>
2024-02-13 13:22:47 +0200

    merge main

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { PrivateComponent } from './layout/private/private.component';
+import { LoginPageComponent } from './pages/signin-page/login-page.component';
 
 const routes: Routes = [
   {
     path: 'private',
     component: PrivateComponent,
-    children: [{ path: 'login', component: LoginPageComponent }],
+    children: [
+      { path: 'login', component: LoginPageComponent },
+      { path: '', component: SignupComponent },
+      { path: 'signup', component: SignupComponent },
+    ],
   },
-
-  { path: '', component: SignupComponent },
-
-  { path: 'signup', component: SignupComponent },
-
-
-
 ];
 
 @NgModule({
