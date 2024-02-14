36afee0a94b158df029f15a75bf92b7c07dd44bd
desicholo <desicholo@gmail.com>
2024-02-14 11:57:39 +0200

    completed signup backend

--- server/Database/db.js
+++ server/Database/db.js
@@ -2,7 +2,7 @@ const Mongoose = require("mongoose")
 
 
 
-const localDB = `mongodb://localhost:27017/role_auth`
+const localDB = `mongodb://localhost:27017/Get-More`
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
     useNewUrlParser: true,
