c5b62129edb5cd4afa0ea2f0cc0118a494a4dc1c
desicholo <desicholo@gmail.com>
2024-02-14 10:58:09 +0200

    stash

--- client/src/app/pages/signup/signup.component.ts
+++ client/src/app/pages/signup/signup.component.ts
@@ -1,10 +1,28 @@
-import { Component } from "@angular/core";
+// signup.component.ts
+
+import { Component } from '@angular/core';
+import { SignupService } from './signup.service';
 
 @Component({
-  templateUrl: 'signup.component.html',
-  styleUrls: ["./signup.component.scss"]
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  
-  constructor() {}
+
+  constructor(private signupService: SignupService) { }
+
+  onSubmit(firstName: string, lastName: string, email: string, password: string): void {
+    this.signupService.signup(firstName, lastName, email, password)
+      .subscribe(
+        (        response: any) => {
+          console.log('Signup successful', response);
+          // Handle successful signup, e.g., redirect to login page
+        },
+        (        error: any) => {
+          console.error('Signup failed', error);
+          // Handle signup error, e.g., display an error message to the user
+        }
+      );
+  }
 }
--- client/src/services/signup.service.ts
+++ client/src/services/signup.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { SignupInterface } from 'src/app/pages/interface/signup';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  userData?: SignupInterface;
+
+  Users: any 
+  router: any;
+  
+
+
+  constructor(private http: HttpClient) { }
+
+  SignupFn(firstName: string, lastName: string, email: string, password: string): Observable<any> {  
+    const body = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+      
+    };
+    
+    return this.http.post<any>('http://localhost:5000/api/signup' ,this.SignupFn );
+  }
+
+  saveToStorage(user: SignupInterface) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUserData() {
+    if (localStorage.getItem('user')) {
+      this.userData = JSON.parse(localStorage.getItem('user') || '');
+    }
+    return this.userData;
+  }
+
+  signin() {
+    localStorage.clear();
+    this.router.navigate(['/signin']);
+  }
+
 }
