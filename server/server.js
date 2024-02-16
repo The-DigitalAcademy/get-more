f41d182e1c801bda030f2a4e2c6651af46f50ce3
Mphoboy <130040248+Mphoboy@users.noreply.github.com>
2024-02-16 13:58:17 +0200

    backend signin and signup works

--- server/server.js
+++ server/server.js
@@ -1,23 +1,29 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const connectDB = require('./Database/db')
-const port = process.env.PORT || 60000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const connectDB = require("./Database/db");
+const port = process.env.PORT || 10000;
 
-connectDB()
+connectDB();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
 
-app.use('/api/users', require('./Routes/routes'))
+app.use(express.json());
 
+app.use("/api", require("./Routes/routes"));
 
-app.get('', (req, res) => {
-    res.sta
-    tus(200).json({server: 'The derver is up'})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from the server" });
+});
+
 app.listen(port, () => {
-    console.log(`server is up http://localhost:${port}`)
-})
+  console.log(`server is up http://localhost:${port}`);
+});
+
+// Handling Error
+process.on("unhandledRejection", (err) => {
+  console.log(`An error occurred: ${err.message}`);
+  server.close(() => process.exit(1));
+});
