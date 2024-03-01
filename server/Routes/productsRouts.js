297d11841a795d1c71f137151793b83a1ec2dc39
Mphoboy <130040248+Mphoboy@users.noreply.github.com>
2024-03-01 14:39:30 +0200

    get one backend done

--- server/Routes/productsRouts.js
+++ server/Routes/productsRouts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {addProduct, getAllProducts } = require('../Controllers/products')
+const {addProduct, getAllProducts, getSingle } = require('../Controllers/products')
 
 
 // add product
@@ -8,7 +8,12 @@ router.post('/create', addProduct)
 
 // //read
 // router.get('/product/',read);
+
+//get all product
 router.get("/all", getAllProducts);
+
+//get one prouduct
+router.get('/single/:id', getSingle);
 
 // //update
 // router.put('/product/',update);
