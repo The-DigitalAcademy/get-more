1939fde7b3c3b21dba273fdbdbd60d2d5e995e93
Mphoboy <130040248+Mphoboy@users.noreply.github.com>
2024-03-13 10:56:22 +0200

    adding cart

--- client/src/app/ui/product/product.component.ts
+++ client/src/app/ui/product/product.component.ts
@@ -8,26 +8,126 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
   product: any;
 
-  constructor(
-    private alertService: AlertService,
+  productData:undefined | product;
+  productQuantity:number=1;
+  removeCart=false;
+  cartData:product|undefined;
+  constructor( private product:ProductService, 
+       private alertService: AlertService,
     private router: Router,
     private route: ActivatedRoute,
     private productService: ProductService
-  ) {}
+    ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
+    let productId= this.route.snapshot.paramMap.get('productId');
+    console.warn(productId);
+    productId && this.productService.getProduct(productId).subscribe((result)=>{
+      this.productData= result;
+      let cartData= localStorage.getItem('localCart');
+      if(productId && cartData){
+        let items = JSON.parse(cartData);
+        items = items.filter((item:product)=>productId=== item.id.toString());
+        if(items.length){
+          this.removeCart=true
+        }else{
+          this.removeCart=false
+        }
+      }
 
-    if (id) {
-      this.productService.getSingleproduct(id).subscribe((data: any) => {
-        this.product = data;
-        console.log(this.product);
-        
-      });
+      let user = localStorage.getItem('user');
+      if(user){
+        let userId= user && JSON.parse(user).id;
+        this.productService.getCartList(userId);
+
+        this.productService.cartData.subscribe((result)=>{
+          let item = result.filter((item:product)=>productId?.toString()===item.productId?.toString())
+       if(item.length){
+        this.cartData=item[0];
+        this.removeCart=true;
+       }
+        })
+      }
+      
+      
+      
+    })
+    
+  }
+  handleQuantity(val:string){
+    if(this.productQuantity<20 && val==='plus'){
+      this.productQuantity+=1;
+    }else if(this.productQuantity>1 && val==='min'){
+      this.productQuantity-=1;
     }
   }
+
+  addToCart(){
+    if(this.productData){
+      this.productData.quantity = this.productQuantity;
+      if(!localStorage.getItem('user')){
+        this.productService.localAddToCart(this.productData);
+        this.removeCart=true
+      }else{
+        let user = localStorage.getItem('user');
+        let userId= user && JSON.parse(user).id;
+        let cartData:cart={
+          ...this.productData,
+          productId:this.productData.id,
+          userId
+        }
+        delete cartData.id;
+        this.product.addToCart(cartData).subscribe((result)=>{
+          if(result){
+           this.productService.getCartList(userId);
+           this.removeCart=true
+          }
+        })        
+      }
+      
+    } 
+  }
+  removeToCart(productId:number){
+    if(!localStorage.getItem('user')){
+this.productService.removeItemFromCart(productId)
+    }else{
+      console.warn("cartData", this.cartData);
+      
+      this.cartData && this.product.removeToCart(this.cartData.id)
+      .subscribe((result)=>{
+        let user = localStorage.getItem('user');
+        let userId= user && JSON.parse(user).id;
+        this.productService.getCartList(userId)
+      })
+    }
+    this.removeCart=false
+  }
+
+
+}
+
+
+  // constructor(
+  //   private alertService: AlertService,
+  //   private router: Router,
+  //   private route: ActivatedRoute,
+  //   private productService: ProductService
+  // ) {}
+
+  // ngOnInit(): void {
+  //   const id = this.route.snapshot.paramMap.get('id');
+
+  //   if (id) {
+  //     this.productService.getSingleproduct(id).subscribe((data: any) => {
+  //       this.product = data;
+  //       console.log(this.product);
+        
+  //     });
+  //   }
+  // }
 
 }
